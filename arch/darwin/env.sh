2f77acc4384a654724b0a9183621962d078d0c21
Marc Charest <charest@lanl.gov>
2016-02-12 14:14:20 -0700

    fixed to work on moonlight

--- arch/darwin/env.sh
+++ arch/darwin/env.sh
@@ -3,40 +3,63 @@
 # placeholder
 #~----------------------------------------------------------------------------~#
 
-compiler=$1
-
-TPL_DEFAULT_PATH=/usr/projects/ngc/public/ale-thirdparty
-TPL_DEFAULT_DOWNLOAD_PATH=${TPL_DEFAULT_PATH}/src
-TPL_DEFAULT_INSTALL_PREFIX=${TPL_DEFAULT_PATH}/builds/gcc/install
-
-if [ "$compiler" == "gcc" ]; then
-  echo "Using GNU environment"
-  module purge
-  module load cmake gcc/5.3.0
-  module list
-  export CC=gcc
-  export CXX=g++  
-  TPL_DEFAULT_INSTALL_PREFIX=${TPL_DEFAULT_PATH}/builds/$compiler/install
-elif [ "$compiler" == "clang" ]; then
-  echo "Using clang environment"  
-  module purge
-  module load cmake clang/3.7.0
-  module list
-  export CC=clang
-  export CXX=clang++ 
-  export CFLAGS=--gcc-toolchain=/projects/opt/centos7/gcc/5.3.0
-  export CXXFLAGS=--gcc-toolchain=/projects/opt/centos7/gcc/5.3.0
-  TPL_DEFAULT_INSTALL_PREFIX=${TPL_DEFAULT_PATH}/builds/$compiler/install
-elif [ "$compiler" == "own" ]; then
-  echo "Using user environment"
-  module list
-else
+# a function to print the usage
+usage(){
   echo "no build environment specified"
   echo "Usage: $(basename $0) env_type"
   echo "env_type : gcc   - use GNU compilers"
   echo "           clang - use clang compilers"
   echo "           own   - let user set their own environment"
   exit 1
+}
+
+
+# set some variables
+compiler=$1
+
+TPL_DEFAULT_PATH=/usr/projects/ngc/public/ale-thirdparty
+TPL_DEFAULT_DOWNLOAD_PATH=${TPL_DEFAULT_PATH}/src
+TPL_DEFAULT_INSTALL_PREFIX=${TPL_DEFAULT_PATH}/builds/gcc/install
+
+# make sure at least one argument
+if [ "$#" -eq 0 ]; then
+  usage
+fi
+
+# use your own build environment
+if [ "$compiler" == "own" ]; then
+
+  echo "Using user environment"
+  module list
+
+# use a preset one
+else
+
+  echo "Using ${compiler^^} environment"
+  module purge
+  module load cmake/3.3.2 boost/1.59.0
+  export BOOST_ROOT=$BOOST_INCLUDES/..
+
+  if [ "$compiler" == "gcc" ]; then
+    module load gcc/5.3.0
+    export CC=gcc
+    export CXX=g++  
+    TPL_DEFAULT_INSTALL_PREFIX=${TPL_DEFAULT_PATH}/builds/$compiler/install
+  elif [ "$compiler" == "clang" ]; then
+    echo "Using clang environment"  
+    module load clang/3.7.0
+    export CC=clang
+    export CXX=clang++ 
+    export CFLAGS=--gcc-toolchain=/projects/opt/centos7/gcc/5.3.0
+    export CXXFLAGS=--gcc-toolchain=/projects/opt/centos7/gcc/5.3.0
+    TPL_DEFAULT_INSTALL_PREFIX=${TPL_DEFAULT_PATH}/builds/$compiler/install
+  else
+    echo "Unknown environment selected"
+    usage
+  fi
+
+  module list
+
 fi
 
 
--- arch/moonlight/env.sh
+++ arch/moonlight/env.sh
@@ -3,27 +3,63 @@
 # placeholder
 #~----------------------------------------------------------------------------~#
 
-TPL_DEFAULT_PATH=/usr/projects/ngc/public/ale-thirdparty
-TPL_DEFAULT_DOWNLOAD_PATH=${TPL_DEFAULT_PATH}/src
-
-if [ "$1" == "gcc" ]; then
-  echo "Using GNU environment"
-  module purge
-  module load cmake gcc/5.3.0
-  module list
-  CC=gcc
-  CXX=g++  
-elif [ "$1" == "own" ]; then
-  echo "Using user environment"
-  module list
-else
+# a function to print the usage
+usage(){
   echo "no build environment specified"
   echo "Usage: $(basename $0) env_type"
   echo "env_type : gcc   - use GNU compilers"
+  echo "           clang - use clang compilers"
   echo "           own   - let user set their own environment"
   exit 1
+}
+
+
+# set some variables
+compiler=$1
+
+TPL_DEFAULT_PATH=/usr/projects/ngc/public/ale-thirdparty
+TPL_DEFAULT_DOWNLOAD_PATH=${TPL_DEFAULT_PATH}/src
+
+
+# make sure at least one argument
+if [ "$#" -eq 0 ]; then
+  usage
 fi
 
+# use your own build environment
+if [ "$compiler" == "own" ]; then
+
+  echo "Using user environment"
+  module list
+
+# use a preset one
+else
+
+  echo "Using ${compiler^^} environment"
+  module purge
+  module load git/2.3.3 cmake/3.0.0 user_contrib boost/1.59.0
+  export BOOST_INCLUDEDIR=$BOOST_INC_DIR
+
+  if [ "$compiler" == "gcc" ]; then
+    module load gcc/5.3.0
+    export CC=gcc
+    export CXX=g++  
+    TPL_DEFAULT_INSTALL_PREFIX=${TPL_DEFAULT_PATH}/builds/$compiler/install
+  elif [ "$compiler" == "clang" ]; then
+    module load clang/3.7.0
+    export CC=clang
+    export CXX=clang++ 
+    export CFLAGS=--gcc-toolchain=/usr/projects/hpcsoft/toss2/common/gcc/5.3.0
+    export CXXFLAGS=--gcc-toolchain=/usr/projects/hpcsoft/toss2/common/gcc/5.3.0
+    TPL_DEFAULT_INSTALL_PREFIX=${TPL_DEFAULT_PATH}/builds/$compiler/install
+  else
+    echo "Unknown environment selected"
+    usage
+  fi
+
+  module list
+
+fi
 
 #~---------------------------------------------------------------------------~-#
 # placeholder
